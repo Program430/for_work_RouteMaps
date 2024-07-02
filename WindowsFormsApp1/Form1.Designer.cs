0a64c4adacc763d271fa78cc5449556e547cac24
Timur <abdtimurrif@gmail.com>
2024-07-02 09:41:03 +0300

    init commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.Location = new System.Drawing.Point(40, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 133);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "СОЗДАТЬ МАРШРУТНЫЕ КАРТЫ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Proxy 1", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Нажмите на кнопку и выберите файл \r\nдля создания маршрутных карт";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 228);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Основные материалы(Лист)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Xls;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using System.Xml.Linq;
+using Spire.Xls.Core;
+using Spire.Pdf.Exporting.XPS.Schema;
+using System.Runtime.Remoting.Messaging;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+using OfficeOpenXml.Drawing;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string filePath2;
+       
+        public Form1()
+        {       
+            InitializeComponent(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.Delete("Duplicatesheet1.xlsx");
+            File.Copy("Duplicatesheet.xlsx", "Duplicatesheet1.xlsx");
+
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            OfficeOpenXml.Drawing.ExcelPicture excelImage;
+            //provide file path
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filePath = openFileDialog1.FileName;
+
+            string systemfile = "System1.xlsx";
+            ExcelPackage excelPackage = new ExcelPackage(systemfile);
+            using (ExcelPackage package = new ExcelPackage(filePath))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                int colCount = worksheet.Dimension.End.Column;  //get Column Count
+                int rowCount = worksheet.Dimension.End.Row;     //get row count
+                int i = 0;
+                for (int row = 2 ; row <= rowCount; row = row + 4)
+                {
+                    for (int row1 = 0; row1 < 4; row1++)
+                    {
+                            string l1,l2,l3,l4,l5,l6;
+
+                            try
+                            {
+                                l1 = worksheet.Cells[row + row1, 4].Value.ToString();
+                                l2 = worksheet.Cells[row + row1, 3].Value.ToString();
+                                l3 = worksheet.Cells[row + row1, 8].Value.ToString();
+                                l4 = worksheet.Cells[row + row1, 6].Value.ToString();
+                                l5 = worksheet.Cells[row + row1, 5].Value.ToString();
+                                l6 = worksheet.Cells[row + row1, 11].Value.ToString();
+                            }
+                            catch
+                            {
+                                l1 = l2 = l3 = l4 = l5 = l6 = "";
+                            }
+
+                            //установить значения листа нового
+
+                            ExcelWorksheet worksheet1 = excelPackage.Workbook.Worksheets[0];
+
+                            //строка колонка
+                            worksheet1.Cells[1 + row1 * 10, 1].Value = l1;
+                            worksheet1.Cells[2 + row1 * 10, 1].Value = l2;
+                            worksheet1.Cells[2 + row1 * 10, 11].Value = l3;
+                            worksheet1.Cells[6 + row1 * 10, 4].Value = l4;
+                            worksheet1.Cells[7 + row1 * 10, 4].Value = l4;
+                            worksheet1.Cells[4 + row1 * 10, 7].Value = l5;
+                        switch (l6)
+                            {
+                                case "06":
+                                case "0б":
+                                    l6 = "Люлька";
+                                    break;
+
+                                case "00":
+                                    l6 = "Общ Вид";
+                                    break;
+
+                                case "01":
+                                    l6 = "РамаОп";
+                                    break;
+                                case "02":
+                                    l6 = "рамаПов";
+                                    break;
+                                case "03":
+                                    l6 = "Стрела";
+                                    break;
+                                case "04":
+                                    l6 = "Электр";
+                                    break;
+                                case "05":
+                                    l6 = "Гидравл";
+                                    break;
+                                case "0700":
+                                    l6 = "Г.цилОбщВида";
+                                    break;
+                                case "0701":
+                                    l6 = "Г.ЦилРамыОп";
+                                    break;
+                                case "703":
+                                    l6 = "Г.цилСтр";
+                                    break;
+                                case "706":
+                                    l6 = "Г.цилЛюльки";
+                                    break;
+                                case "702":
+                                    l6 = "Г.ЦилРамыПов";
+                                    break;
+                                default:
+                                    l6 = "ДАННЫЕ НЕ РАСПОЗНАНЫ";
+                                    break;
+                                    
+                            }
+                            worksheet1.Cells[2 + row1 * 10, 13].Value = l6;
+                     
+                    }
+                    excelPackage.Save();
+
+                    //Load the sample Excel
+                    Workbook workbook = new Workbook();
+                    workbook.LoadFromFile(systemfile);
+                    Workbook workbook2 = new Workbook();
+                    workbook2.LoadFromFile("Duplicatesheet1.xlsx");
+                    //Add worksheet and set its name
+                    workbook2.Worksheets.Add("Copy-"+ i.ToString());
+
+                    //copy worksheet to the new added worksheets
+                    workbook2.Worksheets[i+1].CopyFrom(workbook.Worksheets[0]);
+                     
+                    //Save the Excel workbook.
+                    workbook2.SaveToFile("Duplicatesheet1.xlsx");
+
+                    //напечатать лист
+                    Workbook wb = new Workbook();
+
+                    //Load an Excel file
+                    wb.LoadFromFile(systemfile);
+                    
+                    i++;
+                }
+                
+            }
+            Process.Start("C:/Program Files/Microsoft Office/root/Office16/EXCEL", "Duplicatesheet1.xlsx");
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            filePath2 = openFileDialog2.FileName;
+        }
+    }
+}
